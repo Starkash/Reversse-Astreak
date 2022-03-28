cd87cf5e4203e4a4feed9d8abc91b3c585238576
Admin <Admin@DESKTOP-Q9C1DBG>
2022-03-28 22:16:08 +0530

    Add project files.

---
+++ Prime_Number/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace pyr
+{
+    class raj
+    {
+        static void Main(string[] args)
+        {
+            for (int i =4; i>=1; i--)
+            {
+                for (int j = i; j >=1; j--)
+                    Console.Write(j);
+                Console.WriteLine();
+
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
